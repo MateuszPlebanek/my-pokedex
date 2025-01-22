e73319b3220a82dd56197e246d78902630c8c7e6
MateuszPlebanek <matprocom@gmail.com>
2025-01-23 00:06:16 +0100

    Add state management for Pokémon navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import React, { useState } from 'react';
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
-function App() {
 
   const pokemonList = [
     {
       name: "bulbasaur",
       imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    { 
+      name: "charmander", 
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png" 
+    },
+    { 
+      name: "squirtle",
+     imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+    },
+    { 
+      name: "pikachu",
+     imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
     },
     {
       name: "mew",
@@ -14,10 +26,27 @@ function App() {
     }
   ];
 
+  function App() {
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+
+    const handleNext = () => {
+      if (pokemonIndex < pokemonList.length - 1) {
+        setPokemonIndex(pokemonIndex + 1);
+      }
+    };
+
+    const handlePrevious = () => {
+      if (pokemonIndex > 0) {
+        setPokemonIndex(pokemonIndex -1);
+      }
+    };
+
   return (
     <div>
       {/* Passe le Pokémon 'Mew' comme prop au PokemonCard */}
-      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 && <button onClick={handlePrevious}>Précédent</button>}
+      {pokemonIndex < pokemonList.length - 1 && <button onClick={handleNext}>Suivant</button>}
     </div>
   );
 }
